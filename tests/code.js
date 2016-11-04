6488c38dcc61147e0fa9fc9ba6401f0ab494c268
Jesslyn Landgren <jesslyn.landgren@gmail.com>
2016-11-04 07:10:42 -0400

    Finished exercise solutions, fixed function call error

--- tests/code.js
+++ tests/code.js
@@ -1,20 +1,108 @@
-Card = function(point,suit){
-  this.point = point;
-  this.suit = suit;
-  this.getImageUrl = function(){
-    var cardType = this.point;
-    if(this.point==11){
-      cardType = "jack";
-    }
-    if(this.point==12){
-      cardType = "queen";
-    }
-    if(this.point==13){
-      cardType = "king";
-    }
-    if(this.point==1){
-      cardType = "ace";
-    }
-    return "images/"+cardType+"_of_"+this.suit+".png";
-  }
+function Card(num, suit) {
+    this.point = num;
+    this.suit = suit;
 }
+
+Card.prototype.getImageUrl = function() {
+  var name = this.point;
+  if (this.point === 11) {
+    name = 'jack';
+} else if (this.point === 12) {
+    name = 'queen';
+} else if (this.point === 13) {
+    name = 'king';
+} else if (this.point === 1) {
+    name = 'ace';
+  }
+  return 'images/' + name + '_of_' + this.suit + '.png';
+};
+
+function Hand() {
+    this.cards = [];
+    this.points = 0;
+}
+
+Hand.prototype.addCard = function(Card) {
+    this.cards.push(Card);
+};
+
+Hand.prototype.addCard = function(Card) {
+    this.cards.push(Card);
+};
+
+Hand.prototype.getPoints = function(Card) {
+    for (var idxCard in this.cards){
+        this.points += this.cards[idxCard].point;
+    }
+    return this.points;
+};
+
+function Deck() {
+    this.cards = [];
+    for (var idxNumber = 1; idxNumber <= 13; idxNumber++) {
+      for (var idxSuit = 0; idxSuit < 4; idxSuit++) {
+        if (idxSuit === 0) {
+          suit = "spades";
+        } else if (idxSuit === 1) {
+          suit = "hearts";
+        } else if (idxSuit == 2) {
+          suit = "clubs";
+        } else {
+          suit = "diamonds";
+        }
+        this.cards.push(new Card(idxNumber, suit));
+      }
+    }
+}
+
+Deck.prototype.draw = function() {
+    var card1 = this.cards.pop();
+    return card1;
+};
+
+
+Deck.prototype.shuffle = function() {
+    var shuffled = [];
+    var deckLength = this.cards.length;
+    while (deckLength > 0){
+        var j = Math.floor(Math.random() * deckLength);
+        var randCard = this.cards.splice(j,1);
+        shuffled.push(randCard[0]);
+        deckLength -= 1;
+    }
+    this.cards = shuffled;
+};
+
+Deck.prototype.numCardsLeft = function() {
+    return this.cards.length;
+};
+
+var myCard = new Card(5, 'diamonds');
+console.log(myCard.point);
+console.log(myCard.suit);
+console.log(myCard.getImageUrl());
+
+var myHand = new Hand();
+myHand.addCard(new Card(5, 'diamonds'));
+console.log(myHand);
+
+myHand.addCard(new Card(13, 'spades'));
+console.log(myHand);
+
+console.log(myHand.getPoints());
+
+
+var myDeck = new Deck();
+console.log(myDeck.numCardsLeft());
+
+console.log(myDeck.draw());
+console.log(myDeck.numCardsLeft());
+
+console.log(myDeck.draw());
+console.log(myDeck.numCardsLeft());
+
+// console.log(myDeck);
+myDeck.shuffle();
+// console.log(myDeck);
+
+console.log(myDeck.numCardsLeft());
